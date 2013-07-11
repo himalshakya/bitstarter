0a90004c114f16b71a5c1f09b3a881d165ef6d01
himalshakya <shakyahimal1@gmail.com>
2013-07-11 02:41:19 +0000

    Uncleaned Working Code

--- grader.js
+++ grader.js
@@ -24,8 +24,11 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
+
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URLLINK_DEFAULT = "http://fierce-reaches-1070.herokuapp.com";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -35,6 +38,14 @@ var assertFileExists = function(infile) {
     }
     return instr;
 };
+
+var assertLinkExists = function(link){
+   if(link === undefined){
+       console.log("%s not provided.", link);
+       process.exit(1);
+   }
+   return link;
+}
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
@@ -46,6 +57,11 @@ var loadChecks = function(checksfile) {
 
 var checkHtmlFile = function(htmlfile, checksfile) {
     $ = cheerioHtmlFile(htmlfile);
+    return checkHtmlContent($, checksfile);
+};
+
+var checkHtmlContent = function($, checksfile){
+
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -53,7 +69,7 @@ var checkHtmlFile = function(htmlfile, checksfile) {
         out[checks[ii]] = present;
     }
     return out;
-};
+}
 
 var clone = function(fn) {
     // Workaround for commander.js issue.
@@ -61,14 +77,61 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var fileOrUrl = function(fileVal, urlVal){
+    console.log("File or url");
+    console.log(fileVal);
+    console.log(urlVal);
+    if(fileVal){
+        return fileVal;
+    }else if(urlVal){
+        return urlVal;
+    }else{
+        console.log("Neither file nor Url parameter selected");
+        return false;
+    }
+    return false;
+};
+
+var checkUrlFile = function(urllink, checksfile){
+    rest.get(urllink).on('success', function(result) { 
+        fs.writeFile("MyFile", result
+        , function(err){
+            if(err) throw err;
+            console.log("No error");
+            $ = cheerioHtmlFile("MyFile");
+        var temp = checkHtmlContent($, checksfile);
+        var outJson = JSON.stringify(temp, null, 4);
+        console.log(outJson);
+     //  console.log(temp);
+ //   return temp; //checkHtmlContent($, checksfile);
+        });
+   
+    //   console.log("Writing file ...."); 
+    });
+}
+
 if(require.main == module) {
     program
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html') //, clone(assertFileExists), HTMLFILE_DEFAULT)
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+	.option('-u, --url <url_link>', 'Path to url_link') //, clone(assertLinkExists), URLLINK_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+    var checkFileUrl = fileOrUrl(program.file, program.url);
+    if(!checkFileUrl){
+        program.file = HTMLFILE_DEFAULT;
+    }  
+        
+    if(program.file){
+        var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
+    } else if(program.url){
+        console.log("processing for URL");
+    //    var checkJson =  
+            checkUrlFile(program.url, program.checks);
+    } else {
+        console.log("Uncaught condition");
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
